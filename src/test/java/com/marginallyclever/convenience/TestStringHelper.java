2a3aef849c5d6f5d3fab22ba3461d55b78547228
Emeric Laberge <emericlaberge@gmail.com>
2024-10-06 13:34:45 -0400

    added test for LineSegment.ToString

--- src/test/java/com/marginallyclever/convenience/TestStringHelper.java
+++ src/test/java/com/marginallyclever/convenience/TestStringHelper.java
@@ -1,9 +1,11 @@
 package com.marginallyclever.convenience;
-
 import com.marginallyclever.convenience.helpers.StringHelper;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.Color;
+
 
 public class TestStringHelper {
     @Test
@@ -14,4 +16,13 @@ public class TestStringHelper {
         assertEquals("1000001",StringHelper.paddedHex(0x1000001));
         assertEquals("100000",StringHelper.paddedHex(0x100000));
     }
+
+  @Test 
+  public void LineSegment2DToString(){
+    final Point2D start = new Point2D(0.0,0.0);
+    final Point2D end = new Point2D(1.0,1.0);
+    final Color color = new Color(0,0,0);
+    final LineSegment2D lineSegment = new LineSegment2D(start,end,color);
+    assertEquals("(0.0,0.0)-(1.0,1.0)",lineSegment.toString());
+}
 }
