b39494df63b3d54a3ae29c5e3bea6f7ddd5d8c29
Emeric Laberge <emericlaberge@gmail.com>
2024-10-06 20:05:13 -0400

    Added tests for rotate, trans, walk, dup, and Generator_Polyeder.Transform

--- src/test/java/com/marginallyclever/makelangelo/makeart/turtlegenerator/TurtleGeneratorTest.java
+++ src/test/java/com/marginallyclever/makelangelo/makeart/turtlegenerator/TurtleGeneratorTest.java
@@ -1,11 +1,14 @@
 package com.marginallyclever.makelangelo.makeart.turtlegenerator;
 
+import com.marginallyclever.convenience.Point2D;
 import com.marginallyclever.makelangelo.Translator;
 import com.marginallyclever.util.PreferencesHelper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class TurtleGeneratorTest {
@@ -31,4 +34,58 @@ public class TurtleGeneratorTest {
             }
         }
     }
+
+    @Test 
+    public void testTrans() {
+        Generator_Polyeder.Transform transform = new Generator_Polyeder.Transform();
+        Point2D pt = new Point2D(10, 20);
+        Point2D ptTrans = transform.trans(pt);
+        assertEquals(pt.x, ptTrans.x, 0.001);
+        assertEquals(pt.y, ptTrans.y, 0.001);
+  }
+
+    @Test
+    public void testWalk() {
+        Generator_Polyeder.Transform transform = new Generator_Polyeder.Transform();
+        Point2D pt = new Point2D(10, 20);
+        transform.walk(pt);
+        assertEquals(10, transform.org.x, 0.001);
+        assertEquals(20, transform.org.y, 0.001);
+        
+        transform.dump();
+    }
+
+    @Test
+    public void testRotate() {
+        Generator_Polyeder.Transform transform = new Generator_Polyeder.Transform();
+        transform.rotate(0.5);
+        assertEquals(0.87758256189, transform.x_x, 0.001);
+        assertEquals(0.4794255386, transform.x_y, 0.001);
+        assertEquals(-0.4794255386, transform.y_x, 0.001);
+        assertEquals(0.87758256189, transform.y_y, 0.001);
+        transform.dump();
+    }
+
+
+    @Test
+    public void testTransform() {
+        Generator_Polyeder.Transform transform = new Generator_Polyeder.Transform();
+        Point2D pt = new Point2D(10, 20);
+        transform.walk(pt);
+        transform.rotate(0.5);
+        Generator_Polyeder.Transform transformDup = transform.dup();
+        assertEquals(transform.org.x, transformDup.org.x, 0.001);
+        assertEquals(transform.org.y, transformDup.org.y, 0.001);
+        assertEquals(transform.x_x, transformDup.x_x, 0.001);
+        assertEquals(transform.x_y, transformDup.x_y, 0.001);
+        assertEquals(transform.y_x, transformDup.y_x, 0.001);
+        assertEquals(transform.y_y, transformDup.y_y, 0.001);
+        
+        transform.dump();
+        transformDup.dump();
+    }
+    
 }
+
+
+
